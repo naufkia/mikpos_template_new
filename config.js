1966a2e8b9a0a678d09a32d315243c020923b800
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2023-01-05 00:47:14 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+    "hotspotname":  "HOTSPOT.NET",
+    "runningtext":  "Selamat datang di layanan kami, nikmati layanan unlimited tanpa kuota!",
+    "rotuerid":     "123/123" // router id yang di dapatkan dari mikpos.space
+};
